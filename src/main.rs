f159ac9a2f7c417459b8e3c7e11525d3ad0f6c85
Ryo Hashimoto <ryohashimoto@gmail.com>
2020-06-17 14:33:18 +0900

    Accept command line arguments: status, version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod status;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Available commands: status, version")
+    } else {
+        let mode = &args[1];
+        if mode == "version" {
+            println!("rgt {}", env!("CARGO_PKG_VERSION"))
+        } else if mode == "status" {
+            status::main();
+        }
+    }
 }
