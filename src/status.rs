0632f274bf6cc06ee27596abc856a39ef575b099
Ryo Hashimoto <ryohashimoto@gmail.com>
2020-06-18 22:28:54 +0900

    file_status module

--- src/status.rs
+++ src/status.rs
@@ -1,8 +1,7 @@
 extern crate termion;
+mod file_status;
 
-use git2::Repository;
 use std::io::{stdin, stdout, Write};
-use std::process::Command;
 use termion::cursor;
 use termion::event::{Event, Key};
 use termion::input::TermRead;
@@ -10,23 +9,18 @@ use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
 use termion::{clear, color};
 
-struct FileIndex {
-  status: String,
-  name: String,
-}
-
 pub fn main(path: String) {
   let stdin = stdin();
   let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
   write!(stdout, "{}", clear::All).unwrap();
   write!(stdout, "{}", cursor::Goto(1, 1)).unwrap();
 
-  let name = branch_name(path);
+  let name = file_status::branch_name(path);
   write!(stdout, "{}On branch {}", color::Fg(color::Green), name).unwrap();
   write!(stdout, "\r\n{}", color::Fg(color::Reset)).unwrap();
   write!(stdout, "{}Changes to be commited:", color::Fg(color::Blue)).unwrap();
   write!(stdout, "\r\n{}", color::Fg(color::Reset)).unwrap();
-  for file_index in staged_file_indexes() {
+  for file_index in file_status::staged_file_indexes() {
     write!(
       stdout,
       "{}{}{} ",
@@ -45,7 +39,7 @@ pub fn main(path: String) {
   )
   .unwrap();
   write!(stdout, "\r\n{}", color::Fg(color::Reset)).unwrap();
-  for file_index in modified_file_indexes() {
+  for file_index in file_status::modified_file_indexes() {
     write!(
       stdout,
       "{}{}{} ",
@@ -63,70 +57,4 @@ pub fn main(path: String) {
       return;
     }
   }
-}
-
-fn ref_name(path: String) -> String {
-  let repo = match Repository::open(path) {
-    Ok(repo) => repo,
-    Err(e) => panic!("failed to open: {}", e),
-  };
-  let head = match repo.head() {
-    Ok(head) => head,
-    Err(e) => panic!("failed to open: {}", e),
-  };
-  let ref_name = head.name().unwrap();
-  return ref_name.to_string();
-}
-
-fn branch_name(path: String) -> String {
-  let ref_name = ref_name(path);
-  let pattern = "refs/heads/";
-  if ref_name.starts_with(pattern) {
-    return ref_name.trim_start_matches(pattern).to_string();
-  } else {
-    return ref_name;
-  }
-}
-
-fn staged_files_command_output() -> std::process::Output {
-  let output = Command::new("git")
-    .args(&["diff", "--cached", "--name-status"])
-    .output()
-    .expect("failed to execute the command: git diff --cached --name-status");
-  return output;
-}
-
-fn staged_file_indexes() -> Vec<FileIndex> {
-  let output = staged_files_command_output();
-  return file_indexes_for_output(output);
-}
-
-fn modified_files_command_output() -> std::process::Output {
-  let output = Command::new("git")
-    .args(&["diff", "--name-status"])
-    .output()
-    .expect("failed to execute the command: git diff --cached --name-status");
-  return output;
-}
-
-fn modified_file_indexes() -> Vec<FileIndex> {
-  let output = modified_files_command_output();
-  return file_indexes_for_output(output);
-}
-
-fn file_indexes_for_output(output: std::process::Output) -> Vec<FileIndex> {
-  let result = String::from(std::str::from_utf8(&(output.stdout)).unwrap());
-  let file_results: Vec<&str> = result.split("\n").collect();
-  let mut file_indexes: Vec<FileIndex> = Vec::new();
-  for file_result in file_results.iter() {
-    let status_and_file: Vec<&str> = file_result.split("\t").collect();
-    let status = status_and_file.first().unwrap();
-    let file_name = status_and_file.last().unwrap();
-    let file_index = FileIndex {
-      status: status.to_string(),
-      name: file_name.to_string(),
-    };
-    file_indexes.push(file_index)
-  }
-  return file_indexes;
 }
