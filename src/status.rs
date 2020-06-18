809cc9835ef5bdc7b12806497c060e9c3a14c6f2
Ryo Hashimoto <ryohashimoto@gmail.com>
2020-06-18 15:09:09 +0900

    Show "Changes to be commited" files

--- src/status.rs
+++ src/status.rs
@@ -20,6 +20,20 @@ pub fn main(path: String) {
   let name = branch_name(path);
   write!(stdout, "{}On branch {}", color::Fg(color::Green), name).unwrap();
   write!(stdout, "\r\n{}", color::Fg(color::Reset)).unwrap();
+  write!(stdout, "{}Changes to be commited:", color::Fg(color::Blue)).unwrap();
+  write!(stdout, "\r\n{}", color::Fg(color::Reset)).unwrap();
+  for file_index in staged_file_indexes() {
+    write!(
+      stdout,
+      "{}{}{} ",
+      color::Fg(color::Magenta),
+      file_index.status,
+      color::Fg(color::Reset)
+    )
+    .unwrap();
+    write!(stdout, "{}", file_index.name).unwrap();
+    write!(stdout, "\r\n").unwrap();
+  }
   write!(
     stdout,
     "{}Changes not staged for commit:",
@@ -65,12 +79,34 @@ fn branch_name(path: String) -> String {
   }
 }
 
-fn modified_file_indexes() -> Vec<FileIndex> {
-  let command = Command::new("git")
+fn staged_files_command_output() -> std::process::Output {
+  let output = Command::new("git")
+    .args(&["diff", "--cached", "--name-status"])
+    .output()
+    .expect("failed to execute the command: git diff --cached --name-status");
+  return output;
+}
+
+fn staged_file_indexes() -> Vec<FileIndex> {
+  let output = staged_files_command_output();
+  return file_indexes_for_output(output);
+}
+
+fn modified_files_command_output() -> std::process::Output {
+  let output = Command::new("git")
     .args(&["diff", "--name-status"])
     .output()
-    .expect("failed to execute the command");
-  let result = String::from(std::str::from_utf8(&(command.stdout)).unwrap());
+    .expect("failed to execute the command: git diff --cached --name-status");
+  return output;
+}
+
+fn modified_file_indexes() -> Vec<FileIndex> {
+  let output = modified_files_command_output();
+  return file_indexes_for_output(output);
+}
+
+fn file_indexes_for_output(output: std::process::Output) -> Vec<FileIndex> {
+  let result = String::from(std::str::from_utf8(&(output.stdout)).unwrap());
   let file_results: Vec<&str> = result.split("\n").collect();
   let mut file_indexes: Vec<FileIndex> = Vec::new();
   for file_result in file_results.iter() {
