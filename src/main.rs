c9923958618bf335b7edc8febcd6718a4bb8f722
Ryo Hashimoto <ryohashimoto@gmail.com>
2021-05-14 15:25:43 +0900

    Add options: --version, --help, remove version mode

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Available commands: status, version")
+        println!("Available commands: status, --version, --help")
     } else {
         let mode = &args[1];
-        if mode == "version" {
-            println!("rgt {}", env!("CARGO_PKG_VERSION"))
+        if mode == "--version" || mode == "-v" {
+            println!("rgt {}", env!("CARGO_PKG_VERSION"));
+        } else if mode == "--help" || mode == "-h" {
+            println!("Usage: rgt status");
+            println!("");
+            println!("Options:");
+            println!("  -v, --version   Show version");
+            println!("  -h, --help      Show help");
         } else if mode == "status" {
             if args.len() >= 3 {
                 status::main(args[2].to_string());
